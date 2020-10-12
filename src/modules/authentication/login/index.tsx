430f3c203bb197c2ebf4621be90cbb8d38ad1633
DESKTOP-DJBTNDI\ASUS <nguyenhoanganh12000@gmail.com>
2020-10-13 01:02:08 +0700

    Add ui login

--- src/modules/authentication/login/index.tsx
+++ src/modules/authentication/login/index.tsx
@@ -1,6 +1,6 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 import React, { useState } from 'react';
-import { Text, TextInput, View } from 'react-native';
+import { Image, ScrollView, Text, TextInput, View } from 'react-native';
 import { styles } from './styles';
 import { Button } from './../../../shared/components/button';
 import { LOGIN } from './../../../redux/modules/auth/actions';
@@ -105,51 +105,59 @@ const Login: React.FunctionComponent<Props> = (props: Props) => {
 
     return (
         <>
-            <Formik
-                initialValues={initialFormValue}
-                onSubmit={(values) => {validateForm(values);}}
-                // validationSchema={validationSchema}
-            >
-                {({values, handleChange, errors, handleSubmit, handleBlur}) =>
-                    <React.Fragment>
-                        <View style={styles.container}>
-                            <Text style={styles.textInputLabel}>{I18n.translate('authentication.login.username')}</Text>
-                            <TextInput
-                                style={styles.textInput}
-                                value={values.username}
-                                onChangeText={handleChange('username')}
-                                onBlur={handleBlur('username')}
-                                placeholder={I18n.translate('authentication.login.username-placeholder')}
-                            />
-                            <Text style={styles.textInputLabel}>{I18n.translate('authentication.login.password')}</Text>
-                            <TextInput
-                                secureTextEntry={!isPasswordShown}
-                                style={styles.textInput}
-                                value={values.password}
-                                onChangeText={handleChange('password')}
-                                onBlur={handleBlur('password')}
-                                placeholder={I18n.translate('authentication.login.password-placeholder')}
-                            />
-                            <View style={styles.signInButton}>
-                                <Button title={I18n.translate('authentication.login.signin')} onClick={() => handleSubmit()}/>
+            <ScrollView>
+                <View style={styles.headerContainer}>
+                    <Image source={require("./../../../assets/images/logo.png")} style={styles.logo}/>
+                    <Text style={styles.appTitle}>Life Music</Text>
+                </View>
+
+                <Formik
+                    initialValues={initialFormValue}
+                    onSubmit={(values) => {validateForm(values);}}
+                    // validationSchema={validationSchema}
+                >
+                    {({values, handleChange, errors, handleSubmit, handleBlur}) =>
+                        <React.Fragment>
+                            <View style={styles.bodyContainer}>
+                                <Text style={styles.bodyTitle}>Login To Your Account</Text>
+                                <Text style={styles.textInputLabel}>{I18n.translate('authentication.login.username')}</Text>
+                                <TextInput
+                                    style={styles.textInput}
+                                    value={values.username}
+                                    onChangeText={handleChange('username')}
+                                    onBlur={handleBlur('username')}
+                                    placeholder={I18n.translate('authentication.login.username-placeholder')}
+                                />
+                                <Text style={styles.textInputLabel}>{I18n.translate('authentication.login.password')}</Text>
+                                <TextInput
+                                    secureTextEntry={!isPasswordShown}
+                                    style={styles.textInput}
+                                    value={values.password}
+                                    onChangeText={handleChange('password')}
+                                    onBlur={handleBlur('password')}
+                                    placeholder={I18n.translate('authentication.login.password-placeholder')}
+                                />
+                                <View style={styles.signInButton}>
+                                    <Button title={I18n.translate('authentication.login.signin')} onClick={() => handleSubmit()}/>
+                                </View>
+                                <View style={styles.separator}>
+                                    <View style={styles.separatorLine}/>
+                                    <Text style={styles.separatorLabel}>{I18n.translate('authentication.login.or')}</Text>
+                                    <View style={styles.separatorLine}/>
+                                </View>
+                                <View style={styles.buttonGroup}>
+                                    <FacebookButton onClick={() => handleSignInWithFacebook()} />
+                                    <GoogleButton onClick={() => handleSignInWithGoogle()}/>
+                                </View>
+                                <View style={styles.linkButtonGroup}>
+                                    <LinkButton title={I18n.translate('authentication.login.signup')} onClick={() => handleSignUp()}/>
+                                    <LinkButton title={I18n.translate('authentication.login.forgot-password')} onClick={() => handleForgotPassword()}/>
+                                </View>
                             </View>
-                            <View style={styles.separator}>
-                                <View style={styles.separatorLine}/>
-                                <Text style={styles.separatorLabel}>{I18n.translate('authentication.login.or')}</Text>
-                                <View style={styles.separatorLine}/>
-                            </View>
-                            <View style={styles.buttonGroup}>
-                                <FacebookButton onClick={() => handleSignInWithFacebook()} />
-                                <GoogleButton onClick={() => handleSignInWithGoogle()}/>
-                            </View>
-                            <View style={styles.linkButtonGroup}>
-                                <LinkButton title={I18n.translate('authentication.login.signup')} onClick={() => handleSignUp()}/>
-                                <LinkButton title={I18n.translate('authentication.login.forgot-password')} onClick={() => handleForgotPassword()}/>
-                            </View>
-                        </View>
-                    </React.Fragment>
-                }
-            </Formik>
+                        </React.Fragment>
+                    }
+                </Formik>
+            </ScrollView>
         </>
     );
 };
--- src/modules/authentication/login/styles.ts
+++ src/modules/authentication/login/styles.ts
@@ -1,33 +1,71 @@
 import { StyleSheet, Dimensions } from 'react-native';
+import { styleVars } from '../../../shared/constance/style-variables';
 
 const { width } = Dimensions.get('window');
 
 export const styles = StyleSheet.create({
-    container: {
+    container:{
         flex: 1,
         justifyContent: 'center',
+        backgroundColor: styleVars.white, 
+    },
+    headerContainer:{
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        paddingTop: 40,
+        paddingBottom: 20,
+    },
+    logo:{
+        width: 75.46, 
+        height: 87.56,
+    },
+    appTitle:{
+        paddingTop: 10,
+        fontSize: styleVars.largeFontSize,
+        fontWeight: "bold",
+        color: styleVars.secondaryColor,
+    },
+    bodyContainer: {
+        flex: 1,
+        justifyContent: 'center',
+        padding: 20,
+        paddingTop: 30,
+        paddingBottom: 15,
+        backgroundColor: styleVars.primaryColor,
+        borderTopLeftRadius: 30,
+        borderTopRightRadius: 30,
+    },
+    bodyTitle:{
+        color: styleVars.white,
+        fontSize: styleVars.bigFontSize,
+        fontWeight: "bold",
+        textAlign: 'center',
+        marginBottom: 15,
     },
     error: {
-        fontSize: 10,
-        color: 'red',
+        fontSize: styleVars.smallFontSize,
+        color: styleVars.red,
     },
     textInput: {
+        color: styleVars.white,
+        borderColor: styleVars.greyColor,
         borderWidth: 1,
-        marginHorizontal: 10,
         borderRadius: 10,
         paddingHorizontal: 10,
         marginBottom: 10,
     },
     textInputLabel: {
-        marginHorizontal: 10,
+        fontSize: styleVars.baseFontSize,
+        color: styleVars.white,
         marginVertical: 10,
     },
     signInButton: {
+        fontSize: styleVars.baseFontSize,
         marginVertical: 10,
-        marginHorizontal: 10,
     },
     separator: {
-        marginVertical: 20,
+        marginVertical: 10,
         display: 'flex',
         flexDirection: 'row',
         alignItems: 'center',
@@ -35,21 +73,21 @@ export const styles = StyleSheet.create({
     separatorLine: {
         height: 1,
         flex: 1,
-        backgroundColor: '#000',
+        backgroundColor: styleVars.white,
     },
     separatorLabel: {
-        fontSize: 16,
+        color: styleVars.white,
+        fontSize: styleVars.baseFontSize,
         marginHorizontal: 10,
     },
     buttonGroup: {
         flexDirection: 'row',
         justifyContent: 'space-between',
-        marginHorizontal: (width - 110) / 2,
-        marginBottom: 40,
+        marginHorizontal: (width - 150) / 2,
+        marginBottom: 30,
     },
     linkButtonGroup: {
         flexDirection: 'row',
         justifyContent: 'space-between',
-        marginHorizontal: 20,
     },
 });
