254d7aa176cc31681ff2eaa7a14a20da7977fbf8
DESKTOP-DJBTNDI\ASUS <nguyenhoanganh12000@gmail.com>
2020-10-22 00:34:41 +0700

    Add message server, I18

---
+++ src/shared/constance/messages.ts
@@ -0,0 +1,7 @@
+export const Messages = {
+    Auth:{
+        IncorrectPassword: 'Incorrect password',
+        AccountNotExist: 'This account does not exist',
+        FailToSignIn: 'Fail to sign in'
+    }
+}
