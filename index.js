c479ded72aa699137b542676e66403b1ea6ccce0
DESKTOP-DJBTNDI\ASUS <nguyenhoanganh12000@gmail.com>
2020-11-08 02:28:04 +0700

    Merge 'dev' into 'anhnt/ui-login'

--- index.js
+++ index.js
@@ -5,5 +5,14 @@
 import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
+import messaging from '@react-native-firebase/messaging';
+import TrackPlayer from 'react-native-track-player';
+import TrackService from './src/services/track-player';
+
+messaging().setBackgroundMessageHandler(({ data: { title, message } }) => {
+    // Store notification in local storage
+});
 
 AppRegistry.registerComponent(appName, () => App);
+
+TrackPlayer.registerPlaybackService(() => TrackService);
