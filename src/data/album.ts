489f12e6b8c103f8b6de3a460d1b7a3a64b6d2d3
DESKTOP-DJBTNDI\ASUS <nguyenhoanganh12000@gmail.com>
2020-11-08 21:41:35 +0700

    Add data

---
+++ src/data/album.ts
@@ -0,0 +1,12 @@
+export const album = [
+    {
+        album_id: '190889875',
+        title: 'Chương II (Deluxe Edition)',
+        image_url: 'https://photo-resize-zmp3.zadn.vn/w480_r1x1_jpeg/covers/4/6/4657da81115ba8ae15c5ad2386077d3f_1509619'
+    },
+    {
+        album_id: '150362855',
+        title: 'Tâm Hồn Của Đá',
+        image_url: 'https://photo-resize-zmp3.zadn.vn/w480_r1x1_jpeg/covers/7/f/7fdc1a630c238af0815181f9faa190f5_1285212'
+    },
+]
---
+++ src/data/genre.ts
@@ -0,0 +1,22 @@
+export const genre = [
+    {
+        genre_id: '88362838',
+        name: 'Nhạc Trẻ',
+        image_url: ''
+    },
+    {
+        genre_id: '98362838',
+        name: 'Rap Việt',
+        image_url: ''
+    },
+    {
+        genre_id: '108362838',
+        name: 'Rock Việt',
+        image_url: ''
+    },
+    {
+        genre_id: '118362838',
+        name: 'Nhạc Trữ Tình',
+        image_url: ''
+    },
+]
