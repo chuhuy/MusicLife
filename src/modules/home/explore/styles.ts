1121fe1c13717e4f1bd5eb5c0df31aa9d8d54a39
DESKTOP-DJBTNDI\ASUS <nguyenhoanganh12000@gmail.com>
2020-11-16 18:52:45 +0700

    Fix background explore

--- src/modules/home/explore/styles.ts
+++ src/modules/home/explore/styles.ts
@@ -8,6 +8,7 @@ export const styles = StyleSheet.create({
     },
     container: {
         flex: 1,
+        backgroundColor: styleVars.primaryColor,
         paddingVertical: 20,
         paddingHorizontal: 15
     },
